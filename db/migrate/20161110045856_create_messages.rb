55726b56503ef8c8202cc59f1f77addb50e49326
sylxjtu <sylxjtu@users.noreply.github.com>
2016-11-20 13:04:52 +0800

    Update 20161110045856_create_messages.rb

--- db/migrate/20161110045856_create_messages.rb
+++ db/migrate/20161110045856_create_messages.rb
@@ -1,8 +1,8 @@
 class CreateMessages < ActiveRecord::Migration[5.0]
   def change
     create_table :messages do |t|
-      t.references :from_user, foreign_key: true
-      t.references :to_user, foreign_key: true
+      t.references :from_user, foreign_key: {to_table: :users}
+      t.references :to_user, foreign_key: {to_table: :users}
       t.string :title
       t.text :content
       t.boolean :isread
